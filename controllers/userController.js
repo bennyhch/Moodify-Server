c2fcf7fd156339b0604f8f44e460d160210c0454
Ben Ho <bennyhch@gmail.com>
2022-07-18 12:37:36 +0100

    tested with postman - cookie generated on login && register route

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 const User = require("../models/User");
 const { createToken, attachCookieToResp } = require("../utils/jwt");
+const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
   const { username, password, email } = req.body;
@@ -24,7 +25,27 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("login route");
+  const { email, password } = req.body;
+  if (!password || !email) {
+    throw new CustomError.BadRequestError("Please provide password and email");
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new CustomError.BadRequestError(
+      "Account not existed in the db, registeration required"
+    );
+  }
+
+  let isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Incorrect Password");
+  }
+
+  const token = createToken({ email });
+  attachCookieToResp({ token, res });
+
+  res.status(StatusCodes.OK).json({ msg: "SUccessful login", user });
 };
 
 const logout = async (req, res) => {
@@ -33,7 +54,7 @@ const logout = async (req, res) => {
     maxAge: 1,
     secure: true,
   });
-  res.status(StatusCodes.OK).json({ msg: "Logout" });
+  res.status(StatusCodes.OK).json({ msg: "Logged Out Successfully" });
 };
 
 module.exports = { register, login, logout };
